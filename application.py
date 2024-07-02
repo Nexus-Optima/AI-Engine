70b2c8a82cbd0555e4583f474f3d30c37099fb86
unknown <abhilakshmisra@gmail.com>
2024-07-02 12:21:58 +0530

    Updated with web scraping

--- application.py
+++ application.py
@@ -3,6 +3,7 @@ import matplotlib
 from flask_cors import CORS
 from Execute.execute import forecast_pipeline
 import threading
+from data import fetch_data
 
 application = Flask(__name__)
 cors = CORS(application)
@@ -16,11 +17,23 @@ def format_date(date):
 @application.route('/forecast', methods=['POST'])
 def forecast():
     try:
+        print("Forecast endpoint accessed")
         data = request.json
         commodity_name = data.get('commodity_name')
         forecast_pipeline(commodity_name)
         return jsonify({"message": "Forecasting started for " + commodity_name}), 202
     except Exception as e:
+        print(f"Error: {e}")
+        return jsonify({"error": str(e)}), 500
+
+@application.route('/fetch_data', methods=['POST'])
+def get_data():
+    try:
+        fetch_data()
+        return jsonify({"message":"Data fetched successfully"}),200
+
+    except Exception as e:
+        print(f"Error: {e}")
         return jsonify({"error": str(e)}), 500
 
 
--- data.py
+++ data.py
@@ -3,9 +3,344 @@ from Utils.Data_operations import clean_data, left_join_and_adjust_prices
 import Constants.constants as cts
 import pandas as pd
 import io
+import boto3
+from flask import Flask, request, jsonify
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime,timedelta
+import os
 
+app = Flask(__name__)
+
+os.environ['AWS_ACCESS_KEY_ID'] =Credentials.aws_access_key_id
+os.environ['AWS_SECRET_ACCESS_KEY'] =Credentials.aws_secret_access_key
 S3_UPLOAD_BUCKET_NAME =cts.Commodities.COMMODITIES
 S3_READ_BUCKET_NAME = 'b3llcurve-rawdata'
+
+
+def fetch_data():
+    try:
+        end_date = datetime.now()
+        start_date = end_date - timedelta(days=7)
+        commodity_name = 'cotton'
+
+        china_data = fetch_data_china(start_date, end_date)
+        china_convert_data = fetch_data_china_convert(start_date, end_date)
+        save_data_to_s3(commodity_name, china_data, "China", "China")
+        save_data_to_s3(commodity_name, china_convert_data, "China", "China_convert")
+
+
+        usa_data = fetch_data_usa(start_date, end_date)
+        usa_convert_data = fetch_data_usa_convert(start_date, end_date)
+        save_data_to_s3(commodity_name, usa_data, "USA", "Usa")
+        save_data_to_s3(commodity_name, usa_convert_data, "USA", "Usa_convert")
+
+        india_data = fetch_data_india(start_date, end_date)
+        save_data_to_s3(commodity_name, india_data, "India", "India")
+
+        brazil_data = fetch_data_brazil(start_date, end_date)
+        brazil_convert_data = fetch_data_brazil_convert(start_date, end_date)
+        save_data_to_s3(commodity_name, brazil_data, "brazil", "Brazil")
+        save_data_to_s3(commodity_name, brazil_convert_data, "Conversion", "Convert")
+
+        spot_price_data = fetch_data_spot_price(start_date, end_date)
+        print(spot_price_data)
+        save_data_to_s3(commodity_name, spot_price_data, "Spot_Prices", "Spot_price_data")
+
+        data_conversion(commodity_name)
+
+        return jsonify({"message": "Data fetched, stored, and processed successfully"}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+def fetch_data_china(start_date, end_date):
+    url = "https://www.investing.com/commodities/zce-cotton-no.1-futures-historical-data"
+    response=requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    table = soup.find('table', class_='freeze-column-w-1 w-full overflow-x-auto text-xs leading-4')
+    if not table:
+        raise Exception("No historical data table found on the page")
+
+    table_rows = table.find_all('tr')
+
+    if not table_rows:
+        raise Exception("No table rows found in the historical data table")
+
+    data = []
+    for row in table_rows[1:]:
+        columns = row.find_all('td')
+        if len(columns) == 7:
+            date_str = columns[0].text.strip()
+            date = datetime.strptime(date_str, '%m/%d/%Y')
+            price = float(columns[1].text.replace(',', ''))
+            open_ = float(columns[2].text.replace(',', ''))
+            high = float(columns[3].text.replace(',', ''))
+            low = float(columns[4].text.replace(',', ''))
+            vol = columns[5].text.strip()
+            change = columns[6].text.strip()
+
+            if start_date <= date <= end_date:
+                data.append([date, price, open_, high, low, vol, change])
+
+    if not data:
+        raise Exception("No data points found within the specified date range")
+
+    df = pd.DataFrame(data, columns=['Date', 'Price', 'Open', 'High', 'Low', 'Volume', 'Chg%'])
+    return df
+
+def fetch_data_china_convert(start_date, end_date):
+    url = "https://www.investing.com/currencies/cny-inr-historical-data"
+    response=requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    table = soup.find('table', class_='freeze-column-w-1 w-full overflow-x-auto text-xs leading-4')
+    if not table:
+        raise Exception("No historical data table found on the page")
+
+    table_rows = table.find_all('tr')
+
+    if not table_rows:
+        raise Exception("No table rows found in the historical data table")
+
+    data = []
+    for row in table_rows[1:]:
+        columns = row.find_all('td')
+        if len(columns) == 7:
+            date_str = columns[0].text.strip()
+            date = datetime.strptime(date_str, '%m/%d/%Y')
+            price = float(columns[1].text.replace(',', ''))
+            open_ = float(columns[2].text.replace(',', ''))
+            high = float(columns[3].text.replace(',', ''))
+            low = float(columns[4].text.replace(',', ''))
+            vol = columns[5].text.strip()
+            change = columns[6].text.strip()
+
+            if start_date <= date <= end_date:
+                data.append([date, price, open_, high, low, vol, change])
+
+    if not data:
+        raise Exception("No data points found within the specified date range")
+
+    df = pd.DataFrame(data, columns=['Date', 'Price', 'Open', 'High', 'Low', 'Volume', 'Chg%'])
+    return df
+
+
+def fetch_data_usa(start_date, end_date):
+    url = "https://www.investing.com/commodities/us-cotton-no.2-historical-data"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    table = soup.find('table', class_='freeze-column-w-1 w-full overflow-x-auto text-xs leading-4')
+    if not table:
+        raise Exception("No historical data table found on the page")
+
+    table_rows = table.find_all('tr')
+
+    if not table_rows:
+        raise Exception("No table rows found in the historical data table")
+
+    data = []
+    for row in table_rows[1:]:
+        columns = row.find_all('td')
+        if len(columns) == 7:
+            date_str = columns[0].text.strip()
+            date = datetime.strptime(date_str, '%m/%d/%Y')
+            price = float(columns[1].text.replace(',', ''))
+            open_ = float(columns[2].text.replace(',', ''))
+            high = float(columns[3].text.replace(',', ''))
+            low = float(columns[4].text.replace(',', ''))
+            vol = columns[5].text.strip()
+            change = columns[6].text.strip()
+
+            if start_date <= date <= end_date:
+                data.append([date, price, open_, high, low, vol, change])
+
+    if not data:
+        raise Exception("No data points found within the specified date range")
+
+    df = pd.DataFrame(data, columns=['Date', 'Price', 'Open', 'High', 'Low', 'Volume', 'Chg%'])
+    return df
+
+def fetch_data_usa_convert(start_date, end_date):
+    url = "https://www.investing.com/currencies/usd-inr-historical-data"
+    response=requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    table = soup.find('table', class_='freeze-column-w-1 w-full overflow-x-auto text-xs leading-4')
+    if not table:
+        raise Exception("No historical data table found on the page")
+
+    table_rows = table.find_all('tr')
+
+    if not table_rows:
+        raise Exception("No table rows found in the historical data table")
+
+    data = []
+    for row in table_rows[1:]:
+        columns = row.find_all('td')
+        if len(columns) == 7:
+            date_str = columns[0].text.strip()
+            date = datetime.strptime(date_str, '%m/%d/%Y')
+            price = float(columns[1].text.replace(',', ''))
+            open_ = float(columns[2].text.replace(',', ''))
+            high = float(columns[3].text.replace(',', ''))
+            low = float(columns[4].text.replace(',', ''))
+            vol = columns[5].text.strip()
+            change = columns[6].text.strip()
+
+            if start_date <= date <= end_date:
+                data.append([date, price, open_, high, low, vol, change])
+
+    if not data:
+        raise Exception("No data points found within the specified date range")
+
+    df = pd.DataFrame(data, columns=['Date', 'Price', 'Open', 'High', 'Low', 'Volume', 'Chg%'])
+    return df
+
+def fetch_data_india(start_date, end_date):
+    url = "https://www.investing.com/commodities/cotton-historical-data"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    table = soup.find('table', class_='freeze-column-w-1 w-full overflow-x-auto text-xs leading-4')
+    if not table:
+        raise Exception("No historical data table found on the page")
+
+    table_rows = table.find_all('tr')
+
+    if not table_rows:
+        raise Exception("No table rows found in the historical data table")
+
+    data = []
+    for row in table_rows[1:]:
+        columns = row.find_all('td')
+        if len(columns) == 7:
+            date_str = columns[0].text.strip()
+            date = datetime.strptime(date_str, '%m/%d/%Y')
+            price = float(columns[1].text.replace(',', ''))
+            open_ = float(columns[2].text.replace(',', ''))
+            high = float(columns[3].text.replace(',', ''))
+            low = float(columns[4].text.replace(',', ''))
+            vol = columns[5].text.strip()
+            change = columns[6].text.strip()
+
+            if start_date <= date <= end_date:
+                data.append([date, price, open_, high, low, vol, change])
+
+    if not data:
+        raise Exception("No data points found within the specified date range")
+
+    df = pd.DataFrame(data, columns=['Date', 'Price', 'Open', 'High', 'Low', 'Volume', 'Chg%'])
+    return df
+
+def fetch_data_spot_price(start_date, end_date):
+    url = "http://www.gujcot.com/daily-spot-rate-list.php"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    table = soup.find('table', class_='table table-bordered text-right')
+    if not table:
+        raise Exception("No historical data table found on the page")
+
+    table_rows = table.find_all('tr')
+
+    if not table_rows:
+        raise Exception("No table rows found in the historical data table")
+
+    data = []
+    for row in table_rows[1:]:
+        columns = row.find_all('td')
+        if len(columns) >= 2:
+            date_str = columns[0].text.strip()
+            date = datetime.strptime(date_str, '%d/%m/%Y')
+            shankar_6 = float(columns[1].text.replace(',', ''))
+
+
+            if start_date <= date <= end_date:
+                data.append([date, shankar_6 ])
+
+    if not data:
+        raise Exception("No data points found within the specified date range")
+
+    df = pd.DataFrame(data, columns=['Date', 'Shankar 6 29-3.8-76'])
+    return df
+
+
+def fetch_data_brazil(start_date, end_date):
+    url = "https://www.cepea.esalq.usp.br/en/indicator/cotton.aspx"
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+        'Accept-Language': 'en-US,en;q=0.9',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Connection': 'keep-alive',
+        'Upgrade-Insecure-Requests': '1',
+        'Cache-Control': 'max-age=0'
+    }
+
+    response = requests.get(url, headers=headers)
+    if response.status_code != 200:
+        raise Exception(f"Failed to fetch data from {url}, status code: {response.status_code}")
+
+    soup = BeautifulSoup(response.content, 'html.parser')
+    table = soup.find('table', class_='imagenet-table imagenet-th2 imagenet-col-12')
+    if not table:
+        raise Exception("No historical data table found on the page")
+
+    table_rows = table.find_all('tr')
+
+    if not table_rows:
+        raise Exception("No table rows found in the historical data table")
+
+    data = []
+    for row in table_rows[1:]:
+        columns = row.find_all('td')
+        if len(columns) >= 1:
+            date_str = columns[0].text.strip()
+            date = datetime.strptime(date_str, '%m/%d/%Y')
+            value = float(columns[1].text.replace(',', ''))
+
+            if start_date <= date <= end_date:
+                data.append([date, value])
+
+    if not data:
+        raise Exception("No data points found within the specified date range")
+
+    df = pd.DataFrame(data, columns=['Date', 'Value'])
+    return df
+
+def fetch_data_brazil_convert(start_date, end_date):
+    url = "https://www.investing.com/currencies/usd-inr-historical-data"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    table = soup.find('table', class_='freeze-column-w-1 w-full overflow-x-auto text-xs leading-4')
+    if not table:
+        raise Exception("No historical data table found on the page")
+
+    table_rows = table.find_all('tr')
+
+    if not table_rows:
+        raise Exception("No table rows found in the historical data table")
+
+    data = []
+    for row in table_rows[1:]:
+        columns = row.find_all('td')
+        if len(columns) >= 1:
+            date_str = columns[0].text.strip()
+            date = datetime.strptime(date_str, '%m/%d/%Y')
+            price = float(columns[1].text.replace(',', ''))
+
+            if start_date <= date <= end_date:
+                data.append([date, price])
+
+    if not data:
+        raise Exception("No data points found within the specified date range")
+
+    df = pd.DataFrame(data, columns=['Date', 'Price'])
+    return df
+
+def save_data_to_s3(commodity_name, df, country, data_type):
+    csv_buffer = io.StringIO()
+    df.to_csv(csv_buffer, index=False)
+    s3_resource = boto3.resource('s3')
+    s3_key = f'{commodity_name}/Data/{country}/{data_type}.csv'
+    s3_resource.Object(S3_READ_BUCKET_NAME, s3_key).put(Body=csv_buffer.getvalue())
+
 
 def brazil_conversion(commodity_name):
     data_key = f'{commodity_name}/Data/brazil/Brazil.csv'
@@ -15,12 +350,13 @@ def brazil_conversion(commodity_name):
     df = read_raw_data_from_s3(S3_READ_BUCKET_NAME, data_key)
     convert_df = read_raw_data_from_s3(S3_READ_BUCKET_NAME, convert_key)
 
-    df = clean_data(df, date_format='%d-%m-%Y')
-    convert_df = clean_data(convert_df, date_format='%B %d, %Y')
+    df = clean_data(df, date_format='%Y-%m-%d')
+    convert_df = clean_data(convert_df, date_format='%Y-%m-%d')
 
     try:
-        merged_df = left_join_and_adjust_prices(df, convert_df, ['Date', 'Value'], join_type='inner')
-        merged_df.rename(columns={'Value': 'Value_Brazil_Index'}, inplace=True)
+        df.rename(columns={'Value': 'Price'}, inplace=True)
+        merged_df = left_join_and_adjust_prices(df, convert_df, ['Date', 'Price'], join_type='inner')
+        merged_df.rename(columns={'Price': 'Value_Brazil_Index'}, inplace=True)
         existing_df = read_raw_data_from_s3(S3_UPLOAD_BUCKET_NAME, output_key)
         updated_df = pd.concat([existing_df, merged_df[['Date', 'Value_Brazil_Index']]]).drop_duplicates(subset='Date', keep='last')
         updated_csv_buffer = io.StringIO()
@@ -38,9 +374,10 @@ def cotlook_conversion(commodity_name):
     convert_df = read_raw_data_from_s3(S3_READ_BUCKET_NAME, convert_key)
 
     df = clean_data(df, date_format='%d-%m-%Y')
-    convert_df = clean_data(convert_df, date_format='%B %d, %Y')
+    convert_df = clean_data(convert_df, date_format='%Y-%m-%d')
 
     try:
+        convert_df.rename(columns={'Price': 'Value'}, inplace=True)
         merged_df = left_join_and_adjust_prices(df, convert_df, ['Date', 'Value'], join_type='inner')
         merged_df.rename(columns={'Value': 'Cotlook_A_index'}, inplace=True)
         existing_df = read_raw_data_from_s3(S3_UPLOAD_BUCKET_NAME, output_key)
@@ -59,8 +396,8 @@ def china_conversion(commodity_name):
     df = read_raw_data_from_s3(S3_READ_BUCKET_NAME, data_key)
     convert_df = read_raw_data_from_s3(S3_READ_BUCKET_NAME, convert_key)
 
-    df = clean_data(df, date_format='%b %d, %Y')
-    convert_df = clean_data(convert_df, date_format='%b %d, %Y')
+    df = clean_data(df, date_format='%Y-%m-%d')
+    convert_df = clean_data(convert_df, date_format='%Y-%m-%d')
 
     try:
         merged_df = left_join_and_adjust_prices(df, convert_df, ['Date', 'Price', 'Open', 'High', 'Low'], join_type='left')
@@ -88,8 +425,8 @@ def usa_conversion(commodity_name):
     df = read_raw_data_from_s3(S3_READ_BUCKET_NAME, data_key)
     convert_df = read_raw_data_from_s3(S3_READ_BUCKET_NAME, convert_key)
 
-    df = clean_data(df, date_format='%b %d, %Y')
-    convert_df = clean_data(convert_df, date_format='%b %d, %Y')
+    df = clean_data(df, date_format='%Y-%m-%d')
+    convert_df = clean_data(convert_df, date_format='%Y-%m-%d')
 
     try:
         merged_df = left_join_and_adjust_prices(df, convert_df, ['Date', 'Price', 'Open', 'High', 'Low'], join_type='left')
@@ -114,8 +451,7 @@ def india_conversion(commodity_name):
     output_key = f'{commodity_name}/IND Cotton Futures.csv'
 
     df = read_raw_data_from_s3(S3_READ_BUCKET_NAME, data_key)
-    df = clean_data(df, date_format='%b %d, %Y')
-
+    df = clean_data(df, date_format='%Y-%m-%d')
     try:
         df.sort_values(by='Date', inplace=True)
         df.rename(columns={
@@ -139,10 +475,10 @@ def spot_price_conversion(commodity_name):
     output_key = f'{commodity_name}/Spot Prices.csv'
 
     df = read_raw_data_from_s3(S3_READ_BUCKET_NAME, data_key)
-    df = clean_data(df, date_format='%d-%m-%Y')
+    df = clean_data(df, date_format='%Y-%m-%d')
 
     try:
-        df.rename(columns={'Shankar 6': 'Output'}, inplace=True)
+        df.rename(columns={'Shankar 6 29-3.8-76': 'Output'}, inplace=True)
         existing_df = read_raw_data_from_s3(S3_UPLOAD_BUCKET_NAME, output_key)
         updated_df = pd.concat([existing_df, df[['Date', 'Output']]], ignore_index=True).drop_duplicates(subset='Date', keep='last')
         updated_csv_buffer = io.StringIO()
@@ -159,4 +495,4 @@ def data_conversion(commodity_name):
     spot_price_conversion(commodity_name)
     cotlook_conversion(commodity_name)
 
-data_conversion('cotton')
+# data_conversion('cotton')
